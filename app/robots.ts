89ea64282d64f37a16d4d0c0757c61fbb20df4dd
NattanSilva <natanxboy@gmail.com>
2026-09-16 23:44:05 -0300

    feat: allow full crawler access in robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -1,11 +1,11 @@
-import type { MetadataRoute } from "next";
-import { site } from "@/lib/site";
-
-export const dynamic = "force-static";
+import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [{ userAgent: "*", allow: "/" }],
-    sitemap: `${site.url}/sitemap.xml`,
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://www.stackpulse.com.br/sitemap.xml',
   };
 }
